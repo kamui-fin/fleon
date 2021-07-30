48fccac61a309719f08dba4b954667a767c2832c
Kamui <fin-kamui@pm.me>
2021-07-30 08:45:02 -0500

    feat: integrate cursor with resizing, moving, and focusing

--- src/wm.c
+++ src/wm.c
@@ -12,7 +12,23 @@ xcb_connection_t *conn;
 const xcb_setup_t *setup;
 xcb_screen_t *screen;
 struct client *clients;
+struct client *focused;
 
+/* For resizing and moving with pointer */
+xcb_get_geometry_reply_t *geometry;
+xcb_query_pointer_reply_t *pointer;
+struct client *mouse_on;
+
+struct client *find_client(xcb_window_t w) {
+    struct client *head = clients;
+    while (head != NULL) {
+        if (head->win == w) {
+            return head;
+        }
+        head = head->next;
+    }
+    return NULL;
+}
 
 void client_add(xcb_window_t w) {
     struct client *new_client = malloc(sizeof(struct client));
@@ -35,15 +51,55 @@ void client_move(struct client *c, int x, int y) {
                          XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y, pos);
 }
 
-void client_raise(xcb_window_t win) {
+void client_raise(struct client *c) {
     uint32_t arg[1] = {XCB_STACK_MODE_ABOVE};
-    xcb_configure_window(conn, win, XCB_CONFIG_WINDOW_STACK_MODE, arg);
+    xcb_configure_window(conn, c->win, XCB_CONFIG_WINDOW_STACK_MODE, arg);
 }
 
 void on_map_request(xcb_generic_event_t *e) {
     xcb_map_request_event_t *ev = (xcb_map_request_event_t *)e;
     client_add(ev->window);
     xcb_map_window(conn, ev->window);
+}
+
+void on_button_pressed(xcb_generic_event_t *e) {
+    xcb_button_press_event_t *ev = (xcb_button_press_event_t *)e;
+    mouse_on = find_client(ev->child);
+    geometry = xcb_get_geometry_reply(
+        conn, xcb_get_geometry(conn, mouse_on->win), NULL);
+    pointer =
+        xcb_query_pointer_reply(conn, xcb_query_pointer(conn, screen->root), 0);
+    if (ev->state == XCB_MOD_MASK_1) {
+        xcb_grab_pointer(conn, 0, screen->root,
+                         XCB_EVENT_MASK_BUTTON_RELEASE |
+                             XCB_EVENT_MASK_BUTTON_MOTION,
+                         XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, screen->root,
+                         XCB_NONE, XCB_CURRENT_TIME);
+    } else {
+        focused = mouse_on;
+        xcb_set_input_focus(conn, XCB_INPUT_FOCUS_POINTER_ROOT, focused->win,
+                            XCB_CURRENT_TIME);
+        client_raise(focused);
+        xcb_ungrab_pointer(conn, XCB_CURRENT_TIME);
+    }
+}
+
+void on_button_release(xcb_generic_event_t *e) {
+    xcb_ungrab_pointer(conn, XCB_CURRENT_TIME);
+}
+
+void on_motion_notify(xcb_generic_event_t *e) {
+    xcb_motion_notify_event_t *ev = (xcb_motion_notify_event_t *)e;
+    struct client *c = find_client(ev->child);
+    if (c) {
+        int xdiff = ev->root_x - pointer->root_x;
+        int ydiff = ev->root_y - pointer->root_y;
+        if (ev->state == (XCB_MOD_MASK_1 | XCB_BUTTON_MASK_1)) {
+            client_move(c, geometry->x + xdiff, geometry->y + ydiff);
+        } else if (ev->state == (XCB_MOD_MASK_1 | XCB_BUTTON_MASK_3)) {
+            client_resize(c, geometry->width + xdiff, geometry->height + ydiff);
+        }
+    }
 }
 
 void quit(int status) {
@@ -82,6 +138,9 @@ void initialize() {
                     XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_BUTTON_RELEASE,
                     XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, screen->root,
                     XCB_NONE, XCB_BUTTON_INDEX_3, XCB_MOD_MASK_1);
+    xcb_grab_button(conn, 0, screen->root, XCB_EVENT_MASK_BUTTON_PRESS,
+                    XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, screen->root,
+                    XCB_NONE, 1, XCB_NONE);
     xcb_flush(conn);
 }
 
@@ -91,6 +150,9 @@ int main(int argc, char *argv[]) {
 
     void (*handlers[30])(xcb_generic_event_t *) = {
         [XCB_MAP_REQUEST] = &on_map_request,
+        [XCB_BUTTON_PRESS] = &on_button_pressed,
+        [XCB_BUTTON_RELEASE] = &on_button_release,
+        [XCB_MOTION_NOTIFY] = &on_motion_notify,
     };
 
     while (1) {
--- src/wm.h
+++ src/wm.h
@@ -3,9 +3,7 @@
 #define ROOT_EVENT_MASK                                                        \
     (XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT |                                    \
      XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY | XCB_EVENT_MASK_PROPERTY_CHANGE |     \
-     XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_BUTTON_RELEASE |             \
-     XCB_EVENT_MASK_KEY_PRESS | XCB_EVENT_MASK_KEY_RELEASE |                   \
-     XCB_EVENT_MASK_POINTER_MOTION)
+     XCB_EVENT_MASK_KEY_PRESS | XCB_EVENT_MASK_KEY_RELEASE)
 
 struct client {
     struct client *next;
@@ -14,16 +12,19 @@ struct client {
 
 typedef void(ev_handler_t)(xcb_generic_event_t *);
 
-
 /* Client managment */
-void client_raise(xcb_window_t win);
-void client_move(struct client *c, int x, int y);
-void client_resize(struct client *c, int w, int h);
-void client_kill(struct client *c);
+struct client *find_client(xcb_window_t w);
 void client_add(xcb_window_t w);
+void client_kill(struct client *c);
+void client_resize(struct client *c, int w, int h);
+void client_move(struct client *c, int x, int y);
+void client_raise(struct client *c);
 
 /* Event handling */
 void on_map_request(xcb_generic_event_t *e);
+void on_button_pressed(xcb_generic_event_t *e);
+void on_button_release(xcb_generic_event_t *e);
+void on_motion_notify(xcb_generic_event_t *e);
 
 /* Setup and teardown */
 void initialize();
